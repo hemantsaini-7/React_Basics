b704ccdbb5c655399b046a58181255f0738ec4b0
Hemant Saini <hsainioff@gmail.com>
2020-12-04 12:12:34 +0530

    gI

---
+++ CRUD/.history/src/App_20201104103105.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/add" component={Add}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ CRUD/.history/src/App_20201104103259.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from './Home';
+import Add from './Add'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* <Router>
+        <Switch>
+          <Route exact path="/add" component={Add}/>
+        </Switch>
+      </Router> */}
+    </div>
+  );
+}
+
+export default App;
